93e2f46cc6d893794899c658eb6ef452b502ec11
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-30 00:20:43 +0800

    save directory contents to tree

--- main.go
+++ main.go
@@ -1,7 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/labstack/echo"
+)
+
+type Node struct {
+	Children map[string]*Node
+	Name     string
+	Path     string
+	IsDir    bool
+}
+
+func (n *Node) Add(path string, d fs.DirEntry) {
+	parts := strings.Split(path, "/")
+	node := Node{
+		Children: make(map[string]*Node),
+		Name:     d.Name(),
+		Path:     path,
+		IsDir:    d.IsDir(),
+	}
+
+	if len(parts) == 1 {
+		n.Children[path] = &node
+		return
+	}
+
+	p := n.Children[parts[0]]
+
+	for _, part := range parts[1:] {
+		if p.Children[part] == nil {
+			p.Children[part] = &node
+			break
+		}
+
+		p = p.Children[part]
+	}
+}
+
+type Tree struct {
+	Node Node
+	mu   sync.Mutex
+}
+
+func Print(n Node, indent string) {
+	fmt.Println(indent, n.Name)
+	for _, v := range n.Children {
+		Print(*v, " "+indent)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	root := "./media/"
+	fileSystem := os.DirFS(root)
+	baseNode := Node{
+		Children: make(map[string]*Node),
+		Name:     "root",
+		Path:     ".",
+		IsDir:    true,
+	}
+	var tree Tree
+	tree.Node = baseNode
+
+	err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if path == "." {
+			return nil
+		}
+
+		tree.Node.Add(path, d)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Print(tree.Node, "")
+
+	e := echo.New()
+	e.Static("/media", "media")
+	e.GET("/test/*", func(c echo.Context) error {
+		tree.mu.Lock()
+		defer tree.mu.Unlock()
+
+		var node *Node
+
+		paramValues := strings.Split(c.ParamValues()[0], "/")
+
+		if paramValues[0] != "" {
+			node = tree.Node.Children[paramValues[0]]
+
+			if len(paramValues) > 1 {
+				for _, v := range paramValues[1:] {
+					node = node.Children[v]
+					if !node.IsDir {
+						break
+					}
+				}
+			}
+		} else {
+			node = &tree.Node
+		}
+
+		return c.JSON(http.StatusOK, node)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
